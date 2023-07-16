27c57c6351910c19b9e1eed603b8c9e3c1a6d087
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2023-07-16 21:58:03 +0800

    add default mathjax when nothing in path

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get("*", (req, res) => {
     require('mathjax').init({
       loader: {load: ['input/tex', 'output/svg']}
     }).then((MathJax) => {
-      console.log(decodeURIComponent(req.path.substr(1)));
-      const svg = MathJax.tex2svg(decodeURIComponent(req.path.substr(1)));
+      const tex = decodeURIComponent(req.path.substr(1)) || 'x^2';
+      const svg = MathJax.tex2svg(tex);
       res.set('Content-Type', 'image/svg+xml');
       res.status(200).send(MathJax.startup.adaptor.outerHTML(svg).replace('<mjx-container class="MathJax" jax="SVG" display="true">','').replace('</mjx-container>',''));
     }).catch((err) => res.sendStatus(500));
